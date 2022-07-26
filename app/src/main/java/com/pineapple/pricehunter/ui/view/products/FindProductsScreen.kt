08fc01f61d67010f44a4370ed7058640aaad9e74
Raul Luis <soyrauluis@gmail.com>
2022-07-25 23:18:47 -0400

    Create About and FindProducts screens

---
+++ app/src/main/java/com/pineapple/pricehunter/ui/view/products/FindProductsScreen.kt
@@ -0,0 +1,9 @@
+package com.pineapple.pricehunter.ui.view.products
+
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FindProductsScreen() {
+    Text("Find Products Screen")
+}
