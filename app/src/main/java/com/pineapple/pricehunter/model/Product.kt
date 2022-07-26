5a207f9149f2623d9153b202480c2bf4c26f2d3f
Raul Luis <soyrauluis@gmail.com>
2022-07-26 18:29:27 -0400

    Update Product.kt model

--- app/src/main/java/com/pineapple/pricehunter/model/Product.kt
+++ app/src/main/java/com/pineapple/pricehunter/model/Product.kt
@@ -1,8 +1,13 @@
 package com.pineapple.pricehunter.model
 
+import com.pineapple.pricehunter.ui.viewmodel.ProductsUiState
+
 data class Product(
     val id: String = "",
     val name: String = "",
     val photoUrl: String = "",
     val prices: List<Price> = emptyList(),
 )
+
+fun Product.toProductsUiState(uiState: ProductsUiState) =
+    uiState.copy(id = id, name = name, photoUrl = photoUrl, prices = prices)
