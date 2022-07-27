91683a13e67512b133d4623961f8858acdbe0f94
Raul Luis <soyrauluis@gmail.com>
2022-07-27 02:08:58 -0400

    Update Theme.kt

--- app/src/main/java/com/pineapple/pricehunter/ui/theme/Theme.kt
+++ app/src/main/java/com/pineapple/pricehunter/ui/theme/Theme.kt
@@ -24,7 +24,7 @@ private val DarkColorScheme = darkColorScheme(
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -45,10 +45,11 @@ fun PriceHunterTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+//            val context = LocalContext.current
+//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+//            if (darkTheme) DarkColorScheme else LightColorScheme
+//        }
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
