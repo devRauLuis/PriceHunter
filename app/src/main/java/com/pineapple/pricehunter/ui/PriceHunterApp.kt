d2ddd4ca66346e6bee3ffab695e73f5ee6614fc4
Raul Luis <soyrauluis@gmail.com>
2022-07-26 20:13:38 -0400

    Add .idea, update dependencies

--- app/src/main/java/com/pineapple/pricehunter/ui/PriceHunterApp.kt
+++ app/src/main/java/com/pineapple/pricehunter/ui/PriceHunterApp.kt
@@ -1,7 +1,7 @@
 package com.pineapple.pricehunter.ui
 
 import android.content.res.Resources
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Face
@@ -9,29 +9,25 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.pineapple.pricehunter.common.snackbar.SnackbarManager
+import com.pineapple.pricehunter.ui.navigation.PriceHunterNavDrawer
 import com.pineapple.pricehunter.ui.navigation.Routes
 import com.pineapple.pricehunter.ui.navigation.priceHunterGraph
-import com.pineapple.pricehunter.ui.theme.PriceHunterTheme
+import com.pineapple.pricehunter.ui.viewmodel.AuthViewModel
 import com.pineapple.pricehunter.ui.viewmodel.PriceHunterViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
-import com.pineapple.pricehunter.ui.navigation.PriceHunterNavDrawer
-import com.pineapple.pricehunter.ui.viewmodel.AuthViewModel
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,8 +35,6 @@ fun PriceHunterApp(
     priceHunterViewModel: PriceHunterViewModel = hiltViewModel(),
     authViewModel: AuthViewModel = hiltViewModel()
 ) {
-
-
     val appState = rememberAppState()
     val scope = rememberCoroutineScope()
 //            PriceHunterNavDrawer(drawerState = appState.drawerState)
@@ -65,7 +59,6 @@ fun PriceHunterApp(
                             "Price Hunter", fontSize = 24.sp, fontWeight = FontWeight.Bold
                         )
                     },
-
                     navigationIcon = {
                         IconButton(onClick = {
                             scope.launch {
@@ -88,17 +81,12 @@ fun PriceHunterApp(
 //                        }
                 )
             },
-
             ) { innerPadding ->
-
-
             NavHost(
                 navController = appState.navController,
                 startDestination = Routes.Home.name,
                 modifier = Modifier.padding(innerPadding)
             ) {
-
-
                 priceHunterGraph(appState)
             }
         }
