afd118b2205c5d5069edc2f97e67046f6a89bf31
Raul Luis <soyrauluis@gmail.com>
2022-07-27 17:00:30 -0400

    Update layout

--- app/src/main/java/com/pineapple/pricehunter/ui/view/products/ProductScreen.kt
+++ app/src/main/java/com/pineapple/pricehunter/ui/view/products/ProductScreen.kt
@@ -1,25 +1,40 @@
 package com.pineapple.pricehunter.ui.view.products
 
+import android.app.Dialog
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.pineapple.pricehunter.common.utils.formatToCurrency
+import com.pineapple.pricehunter.model.Product
+import com.pineapple.pricehunter.ui.navigation.Routes
 import com.pineapple.pricehunter.ui.viewmodel.ProductsViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProductScreen(id: String?, viewModel: ProductsViewModel = hiltViewModel()) {
+fun ProductScreen(
+    id: String?,
+    navigate: (String) -> Unit,
+    viewModel: ProductsViewModel = hiltViewModel()
+) {
 
     val uiState = viewModel.uiState
 
@@ -29,10 +44,12 @@ fun ProductScreen(id: String?, viewModel: ProductsViewModel = hiltViewModel()) {
             viewModel.findProduct(id)
         }
     }
+
     Column(
         modifier = Modifier
             .padding(horizontal = 15.dp, vertical = 15.dp)
     ) {
+
         AsyncImage(
             model = uiState.photoUrl,
             contentDescription = uiState.name,
@@ -41,7 +58,7 @@ fun ProductScreen(id: String?, viewModel: ProductsViewModel = hiltViewModel()) {
                 .padding(horizontal = 30.dp),
         )
         Text(
-            uiState.name.toString(),
+            uiState.name.toString().uppercase(),
             style = MaterialTheme.typography.headlineLarge.copy(fontWeight = FontWeight.Bold),
             modifier = Modifier.padding(top = 15.dp)
         )
@@ -61,7 +78,10 @@ fun ProductScreen(id: String?, viewModel: ProductsViewModel = hiltViewModel()) {
                             .padding(15.dp)
                     ) {
                         if (it == sortedPrices[0])
-                            Text(text = "Menor precio", style = MaterialTheme.typography.labelLarge)
+                            Text(
+                                text = "Menor precio",
+                                style = MaterialTheme.typography.labelLarge
+                            )
                         Text(
                             text = formatToCurrency(it.price),
                             style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.SemiBold)
@@ -94,7 +114,7 @@ fun ProductScreen(id: String?, viewModel: ProductsViewModel = hiltViewModel()) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             ElevatedButton(
-                onClick = {},
+                onClick = { navigate("${Routes.AddPrice.name}/${uiState.id}") },
                 colors = ButtonDefaults.elevatedButtonColors(
                     containerColor = MaterialTheme.colorScheme.secondaryContainer,
                     contentColor = MaterialTheme.colorScheme.secondary
