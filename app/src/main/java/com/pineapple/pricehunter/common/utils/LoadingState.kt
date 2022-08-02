9c5b15057f442f273d84578110eb43d8cb97de43
Raul Luis <soyrauluis@gmail.com>
2022-08-02 14:37:14 -0400

    Update loading state

--- app/src/main/java/com/pineapple/pricehunter/common/utils/LoadingState.kt
+++ app/src/main/java/com/pineapple/pricehunter/common/utils/LoadingState.kt
@@ -1,11 +1,16 @@
 package com.pineapple.pricehunter.common.utils
 
+import com.pineapple.pricehunter.common.snackbar.SnackbarManager
+
 data class LoadingState private constructor(val status: Status, val msg: String? = null) {
     companion object {
         val LOADED = LoadingState(Status.SUCCESS)
         val IDLE = LoadingState(Status.IDLE)
         val LOADING = LoadingState(Status.RUNNING)
-        fun error(msg: String?) = LoadingState(Status.FAILED, msg)
+        fun error(msg: String?): LoadingState {
+            SnackbarManager.showMessage(msg ?: "")
+            return LoadingState(Status.FAILED)
+        }
     }
 
     enum class Status {
