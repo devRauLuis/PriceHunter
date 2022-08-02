befca110b9694ace979145a47df22da1207ceeb1
Raul Luis <soyrauluis@gmail.com>
2022-08-02 14:43:00 -0400

    Update Shop model

--- app/src/main/java/com/pineapple/pricehunter/model/Shop.kt
+++ app/src/main/java/com/pineapple/pricehunter/model/Shop.kt
@@ -1,3 +1,8 @@
 package com.pineapple.pricehunter.model
 
-data class Shop(val id: String = "", val name: String = "", val address: String = "")
+data class Shop(
+    val id: String = "",
+    val name: String = "",
+    val address: String = "",
+    val city: String = ""
+)
