b423e5c511b3143f1226535a61b08b82cd19f95d
Raul Luis <soyrauluis@gmail.com>
2022-08-03 01:29:30 -0400

    Add findShop

--- app/src/main/java/com/pineapple/pricehunter/ui/viewmodel/ShopsViewModel.kt
+++ app/src/main/java/com/pineapple/pricehunter/ui/viewmodel/ShopsViewModel.kt
@@ -4,21 +4,15 @@ import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
-import com.google.firebase.Timestamp
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import com.pineapple.pricehunter.common.snackbar.SnackbarManager
-import com.pineapple.pricehunter.common.snackbar.SnackbarMessage
 import com.pineapple.pricehunter.common.utils.LoadingState
-import com.pineapple.pricehunter.model.Price
 import com.pineapple.pricehunter.model.Product
 import com.pineapple.pricehunter.model.Shop
 import com.pineapple.pricehunter.model.service.DbService
 import com.pineapple.pricehunter.model.toProductsUiState
+import com.pineapple.pricehunter.model.toShopUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import java.io.IOException
@@ -30,7 +24,8 @@ data class ShopsUiState(
     val id: String? = "",
     val name: String? = "",
     val address: String? = "",
-    val city: String? = ""
+    val city: String? = "",
+    val products: List<Product> = listOf()
 )
 
 fun ShopsUiState.toShop() = Shop(
@@ -69,8 +64,9 @@ class ShopsViewModel @Inject constructor(
         uiState = uiState.copy(address = newValue)
     }
 
-
-
+    fun setSearchField(newValue: String) {
+        uiState = uiState.copy(searchField = newValue)
+    }
 
     fun findAllShops(nameQuery: String? = "") {
         viewModelScope.launch {
@@ -82,7 +78,30 @@ class ShopsViewModel @Inject constructor(
             } catch (e: IOException) {
                 loadingState.emit(LoadingState.error(e.localizedMessage))
             }
-            Log.d("PRODUCTS VIEW MODEL", uiState.shops.toString())
+            Log.d("SHOPS VIEW MODEL", uiState.shops.toString())
+        }
+    }
+
+    fun findShop(id: String) {
+        viewModelScope.launch {
+            try {
+                loadingState.emit(LoadingState.LOADING)
+                val shop = dbService.getShop(id)
+                var products = dbService.getAllProducts()
+
+                // Filter products to obtain only the ones that belong to the shop
+                products =
+                    products.filter { product -> product.prices.any { price -> price.shopId == id } }
+                        .map { product -> product.copy(prices = product.prices.filter { price -> price.shopId == id }) }
+
+                if (shop != null) {
+                    uiState = shop.toShopUiState(uiState).copy(products = products)
+                }
+                loadingState.emit(LoadingState.LOADED)
+            } catch (e: IOException) {
+                loadingState.emit(LoadingState.error(e.localizedMessage))
+            }
+            Log.d("SHOPS VIEW MODEL", uiState.toShop().toString())
         }
     }
 
@@ -98,7 +117,7 @@ class ShopsViewModel @Inject constructor(
                 } else if (shop.address.isBlank() || shop.address.length < 3) {
                     SnackbarManager.showMessage("La dirección es requerida con mas de 3 letras")
                     return@launch
-                }else if (shop.city.isBlank() || shop.city.length < 3) {
+                } else if (shop.city.isBlank() || shop.city.length < 3) {
                     SnackbarManager.showMessage("La ciudad es requerida con mas de 3 letras")
                     return@launch
                 }
